3efc1264f0e1152421f065716d604b1f0dc39820
ManuelsAguilera <103870236+ManuelsAguilera@users.noreply.github.com>
2023-08-31 20:18:45 +0000

    Configurada coleccion y mini menu

---
+++ Agenda.java
@@ -0,0 +1,99 @@
+import java.util.*;
+public class Agenda {
+    private Map<String, ArrayList<Reunion>> reunions = new HashMap<>();
+    private int[][] month = new int[7][7];
+
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Agregar reunión");
+            System.out.println("2. Eliminar reunión");
+            System.out.println("3. Mostrar reuniones de una fecha");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // Consumir el salto de línea después de leer el número
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Ingrese la fecha (dd-mm-yyyy): ");
+                    String date = scanner.nextLine();
+                    System.out.print("Ingrese el título de la reunión: ");
+                    String title = scanner.nextLine();
+                    System.out.print("Ingrese la descripción de la reunión: ");
+                    String description = scanner.nextLine();
+                    System.out.print("Ingrese la hora de la reunión: ");
+                    int hour = scanner.nextInt();
+                    Reunion reunion = new Reunion(title, description, hour);
+                    agenda.addReunion(date, reunion);
+                    System.out.println("Reunión agregada con éxito.");
+                    break;
+                case 2:
+                    System.out.print("Ingrese la fecha (dd-mm-yyyy): ");
+                    String deleteDate = scanner.nextLine();
+                    System.out.print("Ingrese el título de la reunión a eliminar: ");
+                    String deleteTitle = scanner.nextLine();
+                    agenda.popReunion(deleteDate, deleteTitle);
+                    System.out.println("Reunión eliminada, si existía.");
+                    break;
+                case 3:
+                    System.out.print("Ingrese la fecha (dd-mm-yyyy): ");
+                    String showDate = scanner.nextLine();
+                    agenda.showDate(showDate);
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción inválida, por favor seleccione una opción válida.");
+            }
+		}
+    }
+
+    public void addReunion(String key, Reunion data) {
+        // Verificar si el ArrayList existe
+        if (!reunions.containsKey(key)) {
+            ArrayList<Reunion> nuevoArray = new ArrayList<>();
+            reunions.put(key, nuevoArray);
+        }
+        
+        reunions.get(key).add(data);
+    }
+
+    private int search(ArrayList<Reunion> target, String title) {
+        for (int i = 0; i < target.size(); i++) {
+            if (title.equals(target.get(i).getTitle())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void popReunion(String key, String title) {
+        ArrayList<Reunion> aux = reunions.get(key);
+        if (aux == null) {
+            return;
+        }
+
+        int position = search(aux, title);
+
+        if (position == -1) {
+            return;
+        }
+
+        aux.remove(position);
+    }
+
+    public void showDate(String key) {
+        ArrayList<Reunion> aux = reunions.get(key);
+        if (aux == null) {
+            return;
+        }
+        for (int i = 0; i < aux.size(); i++) {
+            aux.get(i).printR();
+        }
+    }
+}
---
+++ Reunion.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Reunion {
+	private String title;
+	private String descripcion;
+	private int hora;
+
+	public Reunion(String titulo, String descrip, int nuevaHora) {
+		title = titulo;
+		descripcion = descrip;
+		hora = nuevaHora;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public int getHora() {
+		return hora;
+	}
+
+	public void printR() {
+		System.out.println("Titulo: " + title);
+		System.out.println("Descripcion: " + descripcion);
+		System.out.println("Hora: " + hora);
+	}
+}
