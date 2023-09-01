924e3c29b70a578f14014178a5613db7bde688af
ManuelsAguilera <103870236+ManuelsAguilera@users.noreply.github.com>
2023-09-01 23:36:23 +0000

    Agregados instaladores

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Compilando Agenda.java..."
+javac Agenda.java
+
+if [ $? -ne 0 ]; then
+    echo "Error de compilación en Agenda.java."
+    exit 1
+fi
+
+echo "Compilando dayMeeting.java..."
+javac dayMeeting.java
+
+if [ $? -ne 0 ]; then
+    echo "Error de compilación en dayMeeting.java."
+    exit 1
+fi
+
+echo "Ejecutando el programa principal..."
+java MainClass  # Reemplaza "MainClass" con el nombre de tu clase principal
+
+if [ $? -ne 0 ]; then
+    echo "Error al ejecutar el programa."
+    exit 1
+fi
+
+echo "Programa ejecutado con éxito."
